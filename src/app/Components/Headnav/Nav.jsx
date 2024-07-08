34bb78d1154a31fbc56246f7966d301196e2c2bc
ofly153351 <123883715+ofly153351@users.noreply.github.com>
2024-07-08 13:14:14 +0700

    แก้ nav

--- src/app/Components/Headnav/Nav.jsx
+++ src/app/Components/Headnav/Nav.jsx
@@ -1,20 +1,30 @@
+'use client'
 import Link from "next/link"
+import { usePathname } from "next/navigation"
 import React from 'react'
 
+
 function Nav() {
+
+    const pathName = usePathname();
+    // console.log(pathName);
+
     return (
         <>
             <div className="w-screen h-[80px] flex justify-between items-center">
                 <span className="ml-[50px] font-bold text-2xl ">LOGO</span>
                 <div className="gap-5 flex mr-[50px]">
-                    <div>
-                        <Link href='/Login' className="drop-shadow-2xl px-10 py-3 bg-[#0067B3] rounded-md text-[#ffffff] font-bold 
+                    {pathName === '/Login' ? (
+                        <div>
+                            <Link href='/Register' className="drop-shadow-2xl px-10 py-3 bg-[#0067B3] rounded-md text-[#ffffff] font-bold 
+                        hover:bg-gray-200 hover:text-black"><span className="drop-shadow-2xl" >Register</span></Link>
+                        </div>
+                    ) : (
+                        <div>
+                            <Link href='/Login' className="drop-shadow-2xl px-10 py-3 bg-[#0067B3] rounded-md text-[#ffffff] font-bold 
                             hover:bg-gray-200 hover:text-black"><span className="drop-shadow-2xl" >Login</span></Link>
-                    </div>
-                    <div>
-                        <Link href='/Register'className="drop-shadow-2xl px-10 py-3 bg-[#0067B3] rounded-md text-[#ffffff] font-bold 
-                            hover:bg-gray-200 hover:text-black"><span className="drop-shadow-2xl" >Register</span></Link>
-                    </div>
+                        </div>
+                    )}
                 </div>
             </div>
             <hr className="h-[3px] bg-[#0067B3] shadow-md" />
--- src/app/Login/page.jsx
+++ src/app/Login/page.jsx
@@ -3,6 +3,7 @@ import Nav from "../Components/Headnav/Nav";
 import Leftlogin from "../Components/Headnav/Leftlogin";
 
 const page = () => {
+  
   return (
     <div>
       <Nav />
@@ -15,8 +16,8 @@ const page = () => {
 
           <div className="input flex flex-col w-fit static">
             <label
-              for="input"
-              class="text-blue-500 text-xs font-semibold relative top-2 ml-[7px] px-[3px] bg-[#e8e8e8] w-fit"
+              htmlFor="input"
+              className="text-blue-500 text-xs font-semibold relative top-2 ml-[7px] px-[3px] bg-[#e8e8e8] w-fit"
             >
               อีเมล:
             </label>
@@ -25,14 +26,14 @@ const page = () => {
               type="email"
               placeholder=""
               name="input"
-              class="border-blue-500 input px-[10px] py-[11px] text-md bg-[#e8e8e8] border-2 rounded-[5px] w-[400px] focus:outline-none placeholder:text-black/25"
+              className="border-blue-500 input px-[10px] py-[11px] text-md bg-[#e8e8e8] border-2 rounded-[5px] w-[400px] focus:outline-none placeholder:text-black/25"
             />
           </div>
 
           <div className="input flex flex-col w-fit static">
             <label
-              for="input"
-              class="text-blue-500 text-xs font-semibold relative top-2 ml-[7px] px-[3px] bg-[#e8e8e8] w-fit"
+              htmlFor="input"
+              className="text-blue-500 text-xs font-semibold relative top-2 ml-[7px] px-[3px] bg-[#e8e8e8] w-fit"
             >
               รหัสผ่าน:
             </label>
@@ -41,12 +42,12 @@ const page = () => {
               type="password"
               placeholder=""
               name="input"
-              class="border-blue-500 input px-[10px] py-[11px] text-md bg-[#e8e8e8] border-2 rounded-[5px] w-[400px] focus:outline-none placeholder:text-black/25"
+              className="border-blue-500 input px-[10px] py-[11px] text-md bg-[#e8e8e8] border-2 rounded-[5px] w-[400px] focus:outline-none placeholder:text-black/25"
             />
           </div>
 
           <button
-            class="cursor-pointer transition-all bg-blue-500 text-white px-6 py-2 rounded-lg
+            className="cursor-pointer transition-all bg-blue-500 text-white px-6 py-2 rounded-lg
 border-blue-600
 border-b-[4px] hover:brightness-110 hover:-translate-y-[1px] hover:border-b-[6px]
 active:border-b-[2px] active:brightness-90 active:translate-y-[2px]"
