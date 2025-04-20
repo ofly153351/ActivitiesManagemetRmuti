38ccff25bc0494db3c5877fd4d6a8a47cdd95e59
oooooooob <peerapatklintan@obx.local>
2025-04-20 18:49:27 +0700

    23

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const API_BASE = process.env.NEXT_PUBLIC_API_URL;
 const nextConfig = {
+    output: 'standalone', // ✅ เพิ่มบรรทัดนี้
     reactStrictMode: false,
     async rewrites() {
         return [
